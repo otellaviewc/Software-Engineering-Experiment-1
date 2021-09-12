aee0ca85f1a3487da584a3f733131a8f21334d67
otellaviewc <gy_getic@163.com>
2021-09-12 13:56:38 +0800

    提交最大连续子数组和练习

---
+++ MaximumSubarraySum/MaximumSubarraySum.java
@@ -0,0 +1,24 @@
+public class MaximumSubarraySum {
+    public static void main(String[] args) {
+	int[] values = new int[args.length];
+	for (int i = 0; i < args.length; ++i) {
+            values[i] = Integer.parseInt(args[i]);
+	}
+
+        int result = solve(values);
+	System.out.println(result);
+    }
+
+    public static int solve(int[] values) {
+	int max = values[0], sum = 0;
+
+	for (int value : values) {
+	    sum += value;
+
+	    max = Math.max(max, sum);
+	    sum = Math.max(sum, 0);
+	}
+
+	return max;
+    }
+}
