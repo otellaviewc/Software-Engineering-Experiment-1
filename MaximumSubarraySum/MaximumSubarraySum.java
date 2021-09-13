f229fc527d40974f214eddeec678b0ee5ebd4c39
otellaviewc <gy_getic@163.com>
2021-09-13 17:23:08 +0800

    格式化代码

--- MaximumSubarraySum/MaximumSubarraySum.java
+++ MaximumSubarraySum/MaximumSubarraySum.java
@@ -1,24 +1,24 @@
 public class MaximumSubarraySum {
     public static void main(String[] args) {
-	int[] values = new int[args.length];
-	for (int i = 0; i < args.length; ++i) {
+        int[] values = new int[args.length];
+        for (int i = 0; i < args.length; ++i) {
             values[i] = Integer.parseInt(args[i]);
-	}
+        }
 
         int result = solve(values);
-	System.out.println(result);
+        System.out.println(result);
     }
 
     public static int solve(int[] values) {
-	int max = values[0], sum = 0;
+        int max = values[0], sum = 0;
 
-	for (int value : values) {
-	    sum += value;
+        for (int value : values) {
+            sum += value;
 
-	    max = Math.max(max, sum);
-	    sum = Math.max(sum, 0);
-	}
+            max = Math.max(max, sum);
+            sum = Math.max(sum, 0);
+        }
 
-	return max;
+        return max;
     }
 }
--- Prime/Prime.java
+++ Prime/Prime.java
@@ -1,43 +1,43 @@
 public class Prime {
     public static void main(String[] args) {
-	long time0 = System.currentTimeMillis();
+        long time0 = System.currentTimeMillis();
 
-        int[] primes = calcutePrimes(20000);
+        int[] primes = calculatePrimes(20000);
 
-	long time1 = System.currentTimeMillis();
+        long time1 = System.currentTimeMillis();
 
         printPrimes(primes);
 
-	long time2 = System.currentTimeMillis();
+        long time2 = System.currentTimeMillis();
 
-	System.out.printf("\nCalcute: %.1fs, Print: %.1fs\n", (time1 - time0) / 1000.0f, (time2 - time1) / 1000.0f);
+        System.out.printf("\nCalculate: %.1fs, Print: %.1fs\n", (time1 - time0) / 1000.0f, (time2 - time1) / 1000.0f);
     }
 
-    public static int[] calcutePrimes(int n) {
+    public static int[] calculatePrimes(int n) {
         int[] prime = new int[n];
-	boolean[] visit = new boolean[n + 1];
+        boolean[] visit = new boolean[n + 1];
 
-	for (int i = 2; i <= n; ++i) {
+        for (int i = 2; i <= n; ++i) {
             if (!visit[i]) {
                 prime[++prime[0]] = i;
-	    }
+            }
 
-	    for (int j = 1; j <= prime[0] && i * prime[j] <= n; ++j) {
+            for (int j = 1; j <= prime[0] && i * prime[j] <= n; ++j) {
                 visit[i * prime[j]] = true;
 
-		if (i % prime[j] == 0) {
+                if (i % prime[j] == 0) {
                     break;
-		}
-	    }
-	}
+                }
+            }
+        }
 
-	return prime;
+        return prime;
     }
 
     public static void printPrimes(int[] primes) {
-	for (int i = 1; i < primes.length && primes[i] != 0; ++i) {
+        for (int i = 1; i < primes.length && primes[i] != 0; ++i) {
             System.out.print(primes[i]);
             System.out.print(i % 5 == 0 ? '\n' : '\t');
-	}
+        }
     }
 }
